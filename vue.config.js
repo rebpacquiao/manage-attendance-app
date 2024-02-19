7f6ef6e1d06e7814581975c6712f03414b49964c
rebpacquiao <reb.pacquiao@gmail.com>
2024-02-19 21:55:52 +0800

    update public path

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
-  publicPath:
-    process.env.NODE_ENV === "production" ? "/manage-attendance-app/" : "/",
+  publicPath: "/manage-attendance-app/",
   transpileDependencies: true,
   css: {
     loaderOptions: {
