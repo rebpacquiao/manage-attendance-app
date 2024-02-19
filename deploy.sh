98cf05e6d1c6c1d0974701f68df56a93628198b6
rebpacquiao <reb.pacquiao@gmail.com>
2024-02-19 21:46:37 +0800

    attendance

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:rebpacquiao/rebpacquiao.github.io.git main
 
 # if you are deploying to https://rebpacquiao.github.io/manage-attendance-app
-# git push -f git@github.com:rebpacquiao/manage-attendance-app.git main:gh-pages
+# git push -f git@github.com:rebpacquiao/manage-attendance-app.git main:attendance
 
 cd -
--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,6 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   publicPath:
     process.env.NODE_ENV === "production" ? "/manage-attendance-app/" : "/",
-  outputDir: "docs",
   transpileDependencies: true,
   css: {
     loaderOptions: {
